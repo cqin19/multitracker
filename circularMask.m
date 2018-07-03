4790df80372096ec3655e6c89375ae048e65b661
Zach Werkhoven <werkhoven@fas.harvard.edu>
2018-07-03 17:15:31 -0400

    7/3/18

--- circularMask.m
+++ circularMask.m
@@ -1,6 +1,6 @@
 function imHandle = circularMask(radius, imSize)
 bw = false(imSize);
-bw_cen = randi(imSize,1);
+bw_cen = randi(imSize.*imSize,1);
 bw(bw_cen) = 1;
 bw(bwdist(bw)<=radius) = true;
 imHandle = bw;
